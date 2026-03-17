2d6ed6049d217c772b88d55fdbcffe2d7da0cd4f
neeravim <neeravimca@gmail.com>
2026-03-17 16:39:00 +0530

    March 17 updated

---
+++ ep_01/App.js
@@ -0,0 +1,29 @@
+const heading = React.createElement(
+  "h1", // tag name
+  { id: "heading", xyz: "abc" }, // attributes object
+  "Hello ReactJS --- Again", // child | childrens
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  "parent tag",
+  React.createElement("div", { id: "c1" }, [
+    "I'm a child div",
+    React.createElement("h1", { id: "h1" }, "I'm a h1"),
+    React.createElement("h2", { id: "h2" }, "I'm a h2"),
+    React.createElement("h3", { id: "h3" }, "I'm a h3"),
+  ]),
+   React.createElement("div", { id: "c2" }, [
+    "I'm a child div",
+    React.createElement("h1", { id: "h1" }, "C2 -I'm a h1"),
+    React.createElement("h2", { id: "h2" }, "C2 - I'm a h2"),
+    React.createElement("h3", { id: "h3" }, "C2 - I'm a h3"),
+  ]),
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+console.log(parent);
+console.log(root);
+root.render(parent);
