89aa198c2925910c130681eaaf628f9614ed7f39
neeravim <neeravimca@gmail.com>
2026-03-26 22:23:54 +0530

    Css changes and react element , react func component and usage of one in another

--- parcel/ep_03/App.js
+++ parcel/ep_03/App.js
@@ -5,15 +5,53 @@ import ReactDOM from "react-dom/client";
 
 
 // JSX => React.createElement => ReactElement -JSObject => HTMLElement(render)
+// Single line React Element
 const jsxHeading = <h1>Namaste folks, this is a jsxHeading</h1>
 
+const spanText = (
+  <span className="colortale">Span Text color tale</span>
+)
+
+
+const TitleComponent = () => (
+   <h1 className="title">Title Component</h1>
+); 
+const numb = 20000;
+// React functional component
+const HeadingComponent = () => {
+  return <h2 className="redbold">This is React's functional component header</h2>
+}
+
+const SPANH3Component = function(){
+  return (
+    <span>This is H3 component</span>
+  )
+}
+// Multi line React Element
 const jsxMulLineElem = (
 <h1 id="hs"
 className="redbold"
 tabIndex="">
-  Folks, this is a multi line jsx elem
-</h1>
+  Folks, this is a multi line jsx elem {spanText}
+<SPANH3Component/>
+{SPANH3Component()}
+</h1> 
 );
+const HeadingComponent3 = () => {
+  return (
+    <div>
+      <TitleComponent/>
+      <h1 className="redbold">This is react's func comp with explicit return and multi line {numb+30000}</h1>
+      <SPANH3Component/>
+      <HeadingComponent></HeadingComponent>
+      {HeadingComponent()}
+      {console.log("asdfdsfdsf")}
+      {jsxMulLineElem}
+      <p>This is paragraph</p>
+    </div>
+  )
+};
 console.log(jsxMulLineElem);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxMulLineElem);
+root.render(<HeadingComponent3/>);
+// root.render(jsxMulLineElem);
